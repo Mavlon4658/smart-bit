d5202ed72e4751ae1d518d8426736c9a97b173e3
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2024-02-03 13:03:11 +0500

    project ready

---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<template>
+    <div class="container">
+        <h1>hello word</h1>
+    </div>
+</template>
---
+++ src/views/Home..vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name: "Home",
+}
+</script>
+
+<template>
+    <div class="container mx-auto bg-black text-white h-screen">
+        <h1>Home</h1>
+    </div>
+</template>
